d3a57187cfce1303aedc4338ecdc5a1b4e3c5b2a
Roger Holmgren <roger.holmgren@hrmsoftware.se>
2017-12-08 08:37:35 +0100

    Some comments

--- day1.clj
+++ day1.clj
@@ -1,12 +1,11 @@
-(require '[clojure.string :as str])
-
+;; Get input from file to vector of ints
 (def my-input (into [] (map #(Character/getNumericValue %) (str/trim-newline (slurp "day1input")))))
 
 (defn parse [input sum]
   (cond
-    (empty? (rest input)) (println sum)
-    (= (first input) (second input)) (parse (rest input) (+ sum (first input)))
-    :else (parse (rest input) sum)))
+    (empty? (rest input)) (println sum) ;; If no more element return sum
+    (= (first input) (second input)) (parse (rest input) (+ sum (first input))) ;; increase sum in recursive call
+    :else (parse (rest input) sum))) ;; pass on sum in recursive call
 
-(parse (conj my-input (first my-input)) 0 )
+(parse (conj my-input (first my-input)) 0 ) ;; input is with first element added to the end for circular stuff
 
