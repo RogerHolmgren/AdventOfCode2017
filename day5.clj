f3e1dfeac85061a1b98bc335ca620f23a89d5bcb
Roger Holmgren <roger.holmgren@hrmsoftware.se>
2017-12-21 17:57:48 +0100

    Day 5 Part 1, ugly solution

---
+++ day5.clj
@@ -0,0 +1,17 @@
+(use '[clojure.string :only (split)])
+
+(def my-input (into [] (map #(Integer/parseInt %) (split (slurp "day5input") #"\n"))))
+
+(defn nextPointer [input p]
+  (if (> (count input) (+ p (nth input p)))
+    (+ p (nth input p))
+    -1))
+
+(defn parse 
+  [input pointer my-count]
+  (if (= pointer -1)
+    (println my-count)
+   ;; (println (take 10 input) " | pos: " pointer " is increased to " (+ pointer (nth input pointer)) )
+    (recur (assoc input pointer (inc (nth input pointer))) (nextPointer input pointer) (inc my-count))))
+
+(parse my-input 0 0)
